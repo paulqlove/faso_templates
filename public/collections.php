282139357dc0b3ee5cb96b74acbb4c54a7f051dd
Paul Love <paulqlove@gmail.com>
2015-04-27 17:30:00 -0500

    fixed searchbar on collections

--- public/collections.php
+++ public/collections.php
@@ -217,8 +217,8 @@
 							<h3>Search</h3>
 						</div>
 						<div class="row">
-							<div class="cell-md-12 searchbox">
-								<span class="cell-md-12">
+							<div class="cell-xs-12 cell-sm-12 cell-md-12 searchbox">
+								<span class="cell-xs-6 cell-sm-6 cell-md-12">
 								 <input class="searchbox"  type="text" >
 								</span>
 								 <!-- <i class="fa fa-search fa-large"></i> -->
